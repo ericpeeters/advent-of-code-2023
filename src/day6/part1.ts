c159815fb554e66faf973a9dfc1691a612027d72
Eric Peeters <ericpeeters@live.nl>
2023-12-21 21:57:33 +0100

    Added solution for day 6 part 1

--- src/day6/part1.ts
+++ src/day6/part1.ts
@@ -8,15 +8,25 @@ function parseLine(line: string) {
   return line
     .split(" ")
     .filter((x) => !!x)
-    .map(Number);
+    .map(Number)
+    .filter((x) => !isNaN(x));
 }
 
 export function findMarginOfErrorToWinRaces(input = rawInput) {
   const [timeLine, distanceLine] = input.split("\n");
-  const times = parseLine(timeLine.substring(5));
-  const distances = parseLine(distanceLine.substring(8));
+  const times = parseLine(timeLine);
+  const distances = parseLine(distanceLine);
 
-  console.log({ times, distances });
+  const winning = times.map((time, timeIndex) => {
+    return new Array(time).fill(0).filter((_, index) => {
+      const timeLeft = time - index;
+      const raceDistance = index * timeLeft;
+
+      return raceDistance > distances[timeIndex];
+    }).length;
+  });
+
+  return winning.filter((x) => x !== 0).reduce((a, b) => a * b);
 }
 
 /* ========================================================================== */
